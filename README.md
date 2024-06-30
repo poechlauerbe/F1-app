1c52aaf679b8666cf685645d6f65b9971a76ae77
poechlauerbe <poechlauerbe@gmail.com>
2024-06-30 17:09:14 +0200

    added .gitignore and created adapted function to write the drivers into temp/driver.json

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Used API: [OpenF1](https://openf1.org/)
 
+Playing arround with the OpenF1 API to get useful information out of it.
 
+[x] Download player informationn
--- f1-test.js
+++ f1-test.js
@@ -1,10 +1,16 @@
+const fs = require('fs');
+
+
 function driver(num)
 {
 	if(num === 0)
 	{
 		fetch('https://api.openf1.org/v1/drivers?session_key=latest')
   		.then(response => response.json())
-  		.then(jsonContent => console.log(jsonContent));
+  		.then(jsonContent => {
+			// console.log(jsonContent)
+			fs.writeFile('./temp/driver.json', JSON.stringify(jsonContent), 'utf8', () => console.log('File written to ./temp/driver.json'));
+			});
 	}
 	else
 	{
@@ -31,7 +37,7 @@ function position()
 
 
 // 0 for all drivers, number of the driver to get driver data
-// driver(44);
+console.log(driver(0));
 
 
-position();
+// position();
