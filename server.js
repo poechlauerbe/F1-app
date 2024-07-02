7d6755ac44395935cfb78d40d9e4d19968feeb98
poechlauerbe <poechlauerbe@gmail.com>
2024-07-02 19:17:31 +0200

    architecture change to ejs done.

--- server.js
+++ server.js
@@ -26,53 +26,53 @@ app.use('/racecontrol', racecontrolRouter);
 app.use('/teamradio', teamradioRouter);
 
 // API endpoint to fetch and return data
-// app.get('/api/drivers', async (req, res) => {
-//     if (driverDataCache) {
-//         return res.json(driverDataCache);
-//     }
-//     try {
-//         const response = await fetch('https://api.openf1.org/v1/drivers?session_key=latest');
-//         const data = await response.json();
-//         driverDataCache = data;
-//         res.json(data);
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
+app.get('/api/drivers', async (req, res) => {
+    if (driverDataCache) {
+        return res.json(driverDataCache);
+    }
+    try {
+        const response = await fetch('https://api.openf1.org/v1/drivers?session_key=latest');
+        const data = await response.json();
+        driverDataCache = data;
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-// app.get('/api/positions', async (req, res) => {
-//     try {
-//         const response = await fetch('https://api.openf1.org/v1/position?session_key=latest');
-//         const data = await response.json();
-//         res.json(data);
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
+app.get('/api/positions', async (req, res) => {
+    try {
+        const response = await fetch('https://api.openf1.org/v1/position?session_key=latest');
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-// app.get('/api/race_control', async (req, res) => {
-//     try {
-//         const response = await fetch('https://api.openf1.org/v1/race_control?session_key=latest');
-//         const data = await response.json();
-//         res.json(data);
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
+app.get('/api/race_control', async (req, res) => {
+    try {
+        const response = await fetch('https://api.openf1.org/v1/race_control?session_key=latest');
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-// app.get('/api/teamradio', async (req, res) => {
-//     try {
-//         const response = await fetch('https://api.openf1.org/v1/team_radio?session_key=latest');
-//         const data = await response.json();
-//         res.json(data);
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
+app.get('/api/teamradio', async (req, res) => {
+    try {
+        const response = await fetch('https://api.openf1.org/v1/team_radio?session_key=latest');
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
