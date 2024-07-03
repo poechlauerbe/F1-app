a730ba9d6d5a3cc628b6ca59b3eca2b1fa5c87ff
poechlauerbe <poechlauerbe@gmail.com>
2024-07-03 14:40:44 +0200

    added start and end time, corrected styling

--- public/javascript/raceinfo.js
+++ public/javascript/raceinfo.js
@@ -7,12 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
 			const locationElement = document.createElement('p');
 			locationElement.textContent = `Location: ${data[0]['location']} - ${data[0]['country_name']} `;
 			raceinfoDiv.appendChild(locationElement);
+
 			const sessionElement = document.createElement('p');
 			if (data[0]['session_name'] === data[0]['session_type'])
 				sessionElement.textContent = `Session: ${data[0]['session_name']} `;
 			else
 				sessionElement.textContent = `Session: ${data[0]['session_name']} - ${data[0]['session_type']} `;
 			raceinfoDiv.appendChild(sessionElement);
+			const dateElement = document.createElement('p');
+			dateElement.textContent = `Start: ${data[0]['date_start']} \n\n End: ${data[0]['date_end']} `;
+			raceinfoDiv.appendChild(dateElement);
         })
         .catch(error => {
             console.error('Error fetching drivers:', error);
--- routes/raceinfo.js
+++ routes/raceinfo.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render('page01', { title: 'Race info', headline: 'Race info', id: 'raceinfo', script: 'javascript/raceinfo.js'});
+	res.render('page01', { title: 'Race Info', headline: 'Race Info', id: 'raceinfo', script: 'javascript/raceinfo.js'});
 });
 
 module.exports = router;
