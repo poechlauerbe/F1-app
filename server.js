01fb5b67b5644b551ea9a85d3b0d7501d1a9f5e6
Benjamin Pochlauer <bpochlau@c2r5p1.42vienna.com>
2024-07-04 10:34:16 +0200

    problem on different machine with fetch - added import statement

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const app = express();
 const port = 3000;
 let driverDataCache = null;
