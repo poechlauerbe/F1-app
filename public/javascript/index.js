ec80abd0fde3ec6faf3222e9e8aeabd8fc3d71cd
Benjamin Pochlauer <bpochlau@c3r1p1.42vienna.com>
2024-07-19 13:27:21 +0200

    added countdown to index page

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,11 +1,41 @@
-import { options2 } from "./services/mod_options.js";
+import { options, options2 } from "./services/mod_options.js";
+
+let first_item = '';
+
+const countdown = () => {
+	let countdownDiv = document.getElementById('countdown');
+	countdownDiv.innerHTML = '';
+	let startTime = new Date(first_item['start']);
+	let actualTime = new Date();
+	let rest = startTime - actualTime;
+	const days = Math.floor(rest / (24 * 60 * 60 * 1000));
+	const hours = Math.floor((rest % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+	const minutes = Math.floor((rest % (60 * 60 * 1000)) / (60 * 1000));
+	const seconds = Math.floor((rest % (60 * 1000)) / 1000);
+	let timeString = 'Countdown to next event: ';
+	if (days > 0)
+		timeString += days + "days "
+	if (hours < 10)
+	timeString += "0";
+	timeString += hours + ":";
+	if (minutes < 10)
+		timeString += "0";
+	timeString += minutes + ":";
+	if (seconds < 10)
+		timeString += "0";
+	timeString += seconds;
+	if (rest < 0)
+		timeString = "00:00:00";
+	let countdownElem = document.createElement('p');
+	countdownElem.textContent = timeString;
+	countdownDiv.appendChild(countdownElem);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch('/api/schedule')
         .then(response => response.json())
         .then(data => {
 			let i = 0;
-			let location = '';
             const gpListBaseDiv = document.getElementById('next-event');
             gpListBaseDiv.innerHTML = '';
 			const gpListDiv = document.createElement('ul');
@@ -13,8 +43,8 @@ document.addEventListener('DOMContentLoaded', () => {
             data.forEach(event => {
 				if (new Date().toISOString() < event.end && i < 10) {
 					if (i === 0)
-						location = event.location;
-					if (event.location !== location)
+						first_item = event;
+					if (event.location !== first_item.location)
 						return ;
 					const pitElem = document.createElement('li');
 					const date = new Date(event['start']);
@@ -25,6 +55,8 @@ document.addEventListener('DOMContentLoaded', () => {
 					i++;
 				}
             });
+			setInterval(countdown, 1000);
+			countdown();
         })
         .catch(error => {
             console.error('Error fetching pit infos:', error);
--- public/javascript/services/mod_options.js
+++ public/javascript/services/mod_options.js
@@ -1,4 +1,6 @@
 export const options = {
+	month: '2-digit',
+	day: '2-digit',
 	hour: '2-digit',
 	minute: '2-digit',
 	second: '2-digit',
