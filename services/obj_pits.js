aa50eb2b987d5405cfbd79bc769091a967c83a46
Benjamin Pochlauer <bpochlau@c4r5p1.42vienna.com>
2024-07-21 18:57:51 +0200

    made pits object - next step: integrate it

---
+++ services/obj_pits.js
@@ -0,0 +1,28 @@
+function Pits (driverNumber, lap, pitStopTime) {
+  this.driverNumber = driverNumber;
+  this.lap = lap;
+  this.pitStopTime = pitStopTime;
+}
+
+let pitStops = [];
+
+const addPitStop = (driverNumber, lap, pitStopTime) => {
+	const pit = pitStops.find(elem => elem.lap === lap);
+	if (!pit) {
+		pitStops.push(new Pits(driverNumber, lap, pitStopTime))
+	}
+};
+
+const deletePitStops = () => {
+	pitStops = [];
+}
+
+const getPitStops = () => {
+	return pitStops;
+};
+
+module.exports = {
+	addPitStop,
+	deletePitStops,
+	getPitStops
+};
