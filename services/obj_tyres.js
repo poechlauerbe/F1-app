c9c0058a0e4a0e04bddb2d8c673833c7be42c111
poechlauerbe <poechlauerbe@gmail.com>
2024-07-27 19:20:21 +0200

    added deleteTyres and getTyres function to tyres object

--- services/obj_tyres.js
+++ services/obj_tyres.js
@@ -1,20 +1,43 @@
-function Tyres (tyreAgeAtStart, stintNr, lapStart, lapEnd, compound ) {
-	this.tyreAgeAtStart = tyreAgeAtStart;
-	this.stintNr = stintNr;
-	this.lapStart = lapStart;
-	this.lapEnd = lapEnd;
-	this.compound = compound;
-	this.lapCount = this.lapEnd - this.lapStart + lapStart;
+function Tyres(tyreAgeAtStart, stintNr, lapStart, lapEnd, compound) {
+  this.tyreAgeAtStart = tyreAgeAtStart;
+  this.stintNr = stintNr;
+  this.lapStart = lapStart;
+  this.lapEnd = lapEnd;
+  this.compound = compound;
+  this.lapCount = this.lapEnd - this.lapStart + lapStart;
 }
 
-tyres = [];
+let tyres = [];
 
-function addTyre(driverNumber, tyreAgeAtStart, stintNr, lapStart, lapEnd, compound) {
-	if (!tyres[driverNumber]) {
-		tyres.driverNumber = [];
-	}
-	const driverTyres = tyres.driverNumber.find(t => t.stintNr === stintNr);
-	if (!driverTyres) {
-		tyres.driverNumber.push(new Tyres(tyreAgeAtStart, stintNr, lapStart, lapEnd, compound));
-	}
-}
+const addTyre = (
+  driverNumber,
+  tyreAgeAtStart,
+  stintNr,
+  lapStart,
+  lapEnd,
+  compound
+) => {
+  if (!tyres[driverNumber]) {
+    tyres.driverNumber = [];
+  }
+  const driverTyres = tyres.driverNumber.find(t => t.stintNr === stintNr);
+  if (!driverTyres) {
+    tyres.driverNumber.push(
+      new Tyres(tyreAgeAtStart, stintNr, lapStart, lapEnd, compound)
+    );
+  }
+};
+
+const deleteTyres = () => {
+  tyres = [];
+};
+
+const getTyres = driverNumber => {
+  return tyres.driverNumber;
+};
+
+module.exports = {
+  addTyre,
+  deleteTyres,
+  getTyres
+};
