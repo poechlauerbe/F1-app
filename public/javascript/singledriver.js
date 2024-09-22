d2ce75b405b3d4ed84a1db1095a988322f5152ef
poechlauerbe <poechlauerbe@gmail.com>
2024-09-22 10:07:42 +0200

    fixed linter errors

--- public/javascript/singledriver.js
+++ public/javascript/singledriver.js
@@ -86,22 +86,22 @@ const updateObjects = () => {
 };
 
 const getBaseData = () => {
-  const baseData= document.getElementById('base-data');
+  const baseData = document.getElementById('base-data');
   baseData.innerHTML = '';
   if (actualDriver === 0) return;
   const totalApiString = '/api/singledriverBaseData?driverNumber=' + actualDriver;
   fetch(totalApiString)
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
       const driverElem = document.createElement('p');
       driverElem.innerHTML = data;
       baseData.appendChild(driverElem);
       // console.log(data);
-  })
-  .catch(error => {
-    console.error('Error fetching single driver infos:', error);
-  });
-}
+    })
+    .catch(error => {
+      console.error('Error fetching single driver infos:', error);
+    });
+};
 
 const getDrivers = () => {
   fetch('/api/drivers')
